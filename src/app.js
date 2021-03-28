f44917c902f7132b86dc28ed6dc2763b92aae0bb
AJ <alexanderjessediaz@gmail.com>
2021-03-27 18:41:50 -0600

    add html columns, add fed css, start building instantsearch obj

--- src/app.js
+++ src/app.js
@@ -1,23 +1,36 @@
 import Autocomplete from './components/autocomplete';
+import instantsearch from 'instantsearch.js';
+import algoliasearch from 'algoliasearch';
 
+const search = instantsearch({
+  indexName: 'instant_search',
+  searchClient: algoliasearch(
+    'F6UQOMGX2S',
+    '4aee1de08ceb37e9f064f5e22b505eb3'
+    ),
+  });
+  
+  search.start();
+  
 class SpencerAndWilliamsSearch {
   constructor() {
     this._initSearch();
     this._registerEvents();
   }
-
+  
+  
   _initSearch() {
     this.autocompleteDropdown = new Autocomplete();
   }
-
+  
   _registerEvents() {
     const autocomplete = document.querySelector('.autocomplete');
     const searchbox = document.querySelector('#searchbox input');
-
+    
     searchbox.addEventListener('click', () => {
       autocomplete.style.display = 'block';
     });
-
+    
     searchbox.addEventListener('blur', () => {
       autocomplete.style.display = 'none';
     });
@@ -25,3 +38,5 @@ class SpencerAndWilliamsSearch {
 }
 
 const app = new SpencerAndWilliamsSearch();
+
+
